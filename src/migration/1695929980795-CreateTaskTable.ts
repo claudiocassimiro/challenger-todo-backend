4df93607d88c3e2ce6b8b9b8ea580469f6b98d75
Claudio Cassimiro <65298393+claudiocassimiro@users.noreply.github.com>
2023-09-28 17:09:44 -0300

    feat: new migration

---
+++ src/migration/1695929980795-CreateTaskTable.ts
@@ -0,0 +1,67 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from "typeorm";
+
+export class CreateTaskTable1695929980795 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+
+    await queryRunner.createTable(
+      new Table({
+        name: "tasks",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+            generationStrategy: "uuid",
+            default: "uuid_generate_v4()",
+          },
+          {
+            name: "title",
+            type: "varchar",
+          },
+          {
+            name: "description",
+            type: "varchar",
+          },
+          {
+            name: "creation_date",
+            type: "timestamp with time zone",
+          },
+          {
+            name: "completion_date",
+            type: "timestamp with time zone",
+          },
+          {
+            name: "priority",
+            type: "enum",
+            enumName: "priority_enum",
+            enum: ["alta", "média", "baixa"],
+          },
+          {
+            name: "userId",
+            type: "uuid",
+          },
+        ],
+      })
+    );
+
+    await queryRunner.createForeignKey(
+      "tasks",
+      new TableForeignKey({
+        columnNames: ["userId"],
+        referencedTableName: "users",
+        referencedColumnNames: ["id"],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("tasks");
+    await queryRunner.query('DROP EXTENSION "uuid-ossp"');
+  }
+}
