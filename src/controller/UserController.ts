354a701a5f3a140633a25ba779ff754d886a80cc
Claudio Cassimiro <65298393+claudiocassimiro@users.noreply.github.com>
2023-09-28 14:00:47 -0300

    feat: remove unused methods and add more logics on save method

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -5,46 +5,19 @@ import { User } from "../entity/User";
 export class UserController {
   private userRepository = AppDataSource.getRepository(User);
 
-  async all(request: Request, response: Response, next: NextFunction) {
-    return this.userRepository.find();
-  }
+  async save(req: Request, res: Response, next: NextFunction) {
+    const { email, password } = req.body;
 
-  async one(request: Request, response: Response, next: NextFunction) {
-    const id = parseInt(request.params.id);
+    const userExists = await this.userRepository.findOne({ where: { email } });
 
-    const user = await this.userRepository.findOne({
-      where: { id },
-    });
-
-    if (!user) {
-      return "unregistered user";
-    }
-    return user;
-  }
-
-  async save(request: Request, response: Response, next: NextFunction) {
-    const { firstName, lastName, age } = request.body;
-
-    const user = Object.assign(new User(), {
-      firstName,
-      lastName,
-      age,
-    });
-
-    return this.userRepository.save(user);
-  }
-
-  async remove(request: Request, response: Response, next: NextFunction) {
-    const id = parseInt(request.params.id);
-
-    let userToRemove = await this.userRepository.findOneBy({ id });
-
-    if (!userToRemove) {
-      return "this user not exist";
+    if (userExists) {
+      return res.sendStatus(409);
     }
 
-    await this.userRepository.remove(userToRemove);
+    const user = this.userRepository.create({ email, password });
 
-    return "user has been removed";
+    await this.userRepository.save(user);
+
+    return res.json(user);
   }
 }
