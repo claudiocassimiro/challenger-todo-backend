9a632083350419faf77d786da3d67d1d9b6aa63d
Claudio Cassimiro <65298393+claudiocassimiro@users.noreply.github.com>
2023-09-28 14:14:55 -0300

    refactor: simplify router system

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,13 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
-import { Request, Response } from "express";
 import { AppDataSource } from "./data-source";
-import { Routes } from "./routes";
+import routes from "./routes";
 
 AppDataSource.initialize()
   .then(async () => {
     const app = express();
     app.use(bodyParser.json());
-
-    Routes.forEach((route) => {
-      (app as any)[route.method](
-        route.route,
-        (req: Request, res: Response, next: Function) => {
-          const result = new (route.controller as any)()[route.action](
-            req,
-            res,
-            next
-          );
-          if (result instanceof Promise) {
-            result.then((result) =>
-              result !== null && result !== undefined
-                ? res.send(result)
-                : undefined
-            );
-          } else if (result !== null && result !== undefined) {
-            res.json(result);
-          }
-        }
-      );
-    });
+    app.use(routes);
 
     app.listen(3000);
 
