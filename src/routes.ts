d6147f6e90155253b4879815f656243850325bbc
Claudio Cassimiro <65298393+claudiocassimiro@users.noreply.github.com>
2023-09-28 15:49:57 -0300

    feat: add authMiddleware on mock router

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,14 @@
-import UserController from "./controller/UserController";
 import { Router } from "express";
+
+import authMiddleware from "./middlewares/authMiddleware";
+
+import UserController from "./controller/UserController";
+import AuthController from "./controller/AuthController";
 
 const router = Router();
 
 router.post("/users", UserController.save);
+router.post("/login", AuthController.authenticate);
+router.get("/tasks", authMiddleware, UserController.allTasks);
 
 export default router;
