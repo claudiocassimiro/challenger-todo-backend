b92ab3244769fcd3ce7b4e8eb845df613176aaa8
Claudio Cassimiro <65298393+claudiocassimiro@users.noreply.github.com>
2023-09-28 21:00:14 -0300

    feat: implements update method to update tasks

--- src/controller/TaskController.ts
+++ src/controller/TaskController.ts
@@ -3,6 +3,7 @@ import { NextFunction, Request, Response } from "express";
 import { Task } from "../entity/Task";
 import { User } from "../entity/User";
 import { TaskSchema } from "../validations/TaskValidation/createTaskValidationSchema";
+import { Repository } from "typeorm";
 
 class TaskController {
   async save(req: Request, res: Response, next: NextFunction) {
@@ -49,7 +50,7 @@ class TaskController {
 
       return res.status(200).json(task);
     } catch (error) {
-      return res.json({ error });
+      return res.status(400).json({ error });
     }
   }
 
@@ -68,7 +69,7 @@ class TaskController {
 
       return res.status(200).json(tasks);
     } catch (error) {
-      return res.json({ error });
+      return res.status(400).json({ error: error.message });
     }
   }
 
@@ -88,7 +89,50 @@ class TaskController {
 
       res.sendStatus(204);
     } catch (error) {
-      return res.json({ error });
+      return res.status(400).json({ error: error.message });
+    }
+  }
+
+  async update(req: Request, res: Response, next: NextFunction) {
+    const taskRepository = AppDataSource.getRepository(Task);
+    const { id, title, description, completion_data, priority } = req.body;
+
+    try {
+      if (!id) {
+        throw new Error("O id da task a ser atualizada é obrigatório");
+      }
+
+      if (!title && !description && !completion_data && !priority) {
+        throw new Error("Envie ao menos um campo para ser atualizado");
+      }
+
+      const columnsToBeUpdated = {};
+
+      if (title) {
+        columnsToBeUpdated["title"] = title;
+      }
+
+      if (description) {
+        columnsToBeUpdated["description"] = description;
+      }
+
+      if (completion_data) {
+        columnsToBeUpdated["completion_data"] = new Date(completion_data);
+      }
+
+      if (priority) {
+        columnsToBeUpdated["priority"] = priority;
+      }
+
+      console.log("columnsToBeUpdated: ", columnsToBeUpdated);
+
+      await taskRepository.update(id, columnsToBeUpdated);
+
+      const updatedTask = await taskRepository.findOne({ where: { id } });
+
+      return res.status(200).json({ updatedTask });
+    } catch (error) {
+      return res.status(400).json({ error: error.message });
     }
   }
 }
