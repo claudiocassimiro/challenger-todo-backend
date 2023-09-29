7cd29bd1ca11f9500096302b96d3ee03441684c3
Claudio Cassimiro <65298393+claudiocassimiro@users.noreply.github.com>
2023-09-29 10:32:38 -0300

    fix: add dynamic port

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import * as express from "express";
 import * as bodyParser from "body-parser";
 import { AppDataSource } from "./data-source";
 import routes from "./routes";
+import "dotenv/config";
 
 AppDataSource.initialize()
   .then(async () => {
@@ -9,8 +10,10 @@ AppDataSource.initialize()
     app.use(bodyParser.json());
     app.use(routes);
 
-    app.listen(3000);
+    app.listen(process.env.PORT || 3001);
 
-    console.log("Express server has started on port 3000.");
+    console.log(
+      `Express server has started on port ${process.env.PORT || 3001}.`
+    );
   })
   .catch((error) => console.log(error));
